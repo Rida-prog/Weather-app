f50a650debc9b1fccfaa2fb0267cb557bf975fe0
Rida Fatima <ridafatimanasir1@gmail.com>
2026-05-22 13:36:46 +0000

    weather app setup

--- javasrcipt Learn/Variable.js
+++
@@ -1,8 +0,0 @@
-var myName = "Rida" // var can be reassigned or changed
-myNAME = "Fatima"
- 
-let myAge = "18" // let can be reassigned or changed(but cannot be redeclared in the same scope)
-myAge = "19"
-
-const myCity = "Bahawalnagar" // const cannot be reassigned or changed
-console.log(myCity)
